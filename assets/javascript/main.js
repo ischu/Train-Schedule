97bf50442121d696095e025f226d4fc0b6e2929b
ianschu <ianschu7@gmail.com>
2018-12-15 16:13:55 -0500

    basic click function

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,5 +1,5 @@
-$(document).ready(function () {
-});
+// $(document).ready(function () {
+// });
 // Initialize Firebase
 var config = {
     apiKey: "AIzaSyAsR908qTP52Ik_-OlkiDu_xxWFJOQiEFs",
@@ -11,3 +11,26 @@ var config = {
 };
 firebase.initializeApp(config);
 var database = firebase.database();
+
+$("form button").on("click", function(event){
+    event.preventDefault();
+    console.log("choooooo-chooooooooo");
+    getVal = function(x){
+        return $("#"+x+"Input").val().trim();
+    }
+    // input data assigned to variables
+    let newName = getVal("name");
+    let newDest = getVal("dest");
+    let newArrival = getVal("arrival");
+    let newFreq = getVal("freq");
+    // add data to firebase
+    database.ref().push({
+        Train_Name: newName,
+        Destination: newDest,
+        First_Arrival: newArrival,
+        Frequency: newFreq,
+    });
+    console.log(newName, newDest, newArrival, newFreq);
+    // clears form inputs
+    $("form").trigger("reset");
+});
