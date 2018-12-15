717a2a3bf0263930a6d4d5c41821e5b43be80230
ianschu <ianschu7@gmail.com>
2018-12-15 15:11:20 -0500

    frames

---
+++ assets/javascript/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+});
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyAsR908qTP52Ik_-OlkiDu_xxWFJOQiEFs",
+    authDomain: "train-time-baby.firebaseapp.com",
+    databaseURL: "https://train-time-baby.firebaseio.com",
+    projectId: "train-time-baby",
+    storageBucket: "",
+    messagingSenderId: "597322626556"
+};
+firebase.initializeApp(config);
+var database = firebase.database();
